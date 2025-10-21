bd3c7c67b29c70b22116fa001f0cd5826390f362
potdig <potadig.ashnerd.zino@gmail.com>
2025-10-21 12:34:47 +0900

    fix err

--- src/api/screenshots.rs
+++ src/api/screenshots.rs
@@ -2,6 +2,7 @@ use napi_derive::napi;
 
 #[napi]
 pub mod screenshots {
+    use napi::Error;
     use steamworks::screenshots::ScreenshotHandle;
     use steamworks::screenshots::ScreenshotLibraryAddError;
     use std::path::Path;
@@ -11,14 +12,13 @@ pub mod screenshots {
         thumbnail_filename: Option<String>,
         width: i32,
         height: i32
-    ) -> Result<ScreenshotHandle, ScreenshotLibraryAddError> {
+    ) -> Result<ScreenshotHandle, Error> {
         let client = crate::client::get_client();
         client.screenshots().add_screenshot_to_library(
             Path::new(&filename),
             thumbnail_filename.map(|f| Path::new(&f)),
             width,
             height
-        )
+        ).map_err(|e| Error::from_reason(e.to_string()))
     }
-
 }
